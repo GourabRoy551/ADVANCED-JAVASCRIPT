b57460cfcdff48484dadf7ed16f793fee30cba2b
GourabRoy551 <gourabroysec553@gmail.com>
2020-03-08 12:00:07 +0600

    object-property

---
+++ object-property.js
@@ -0,0 +1,9 @@
+const students = [
+    { id: 34789, name: "sjhdf" },
+    { id: 34789, name: "sjhdf" },
+    { id: 34789, name: "sjhdf" },
+    { id: 34789, name: "sjhdf" },
+];
+
+const ID = students.map(s => s.id);
+console.log(ID);
